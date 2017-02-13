4ea8233ea7e94a983d4bf6634aa4e5ca1baa1f0d
u1m0 <caio.quinilato@gmail.com>
2017-02-13 19:55:00 -0200

    Added chef recipes to install elasticsearch, and configured vagrant file to one node

---
+++ elasticsearch/elasticsearch-cluster-5.2/cookbooks/elasticsearch/metadata.rb
@@ -0,0 +1,7 @@
+name             'elasticsearch'
+maintainer       'u1m0'
+maintainer_email 'caio.quinilato@gmail.com'
+license          'GNU'
+description      'Installs/Configures elasticsearch'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ elasticsearch/elasticsearch-cluster-5.2/cookbooks/java8/metadata.rb
@@ -0,0 +1,7 @@
+name             'java8'
+maintainer       'u1m0'
+maintainer_email 'caio.quinilato@gmail.com'
+license          'GNU'
+description      'Installs java8'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
