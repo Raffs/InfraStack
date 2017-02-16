80cdb71d6a962f57a3f35c9ecb52523ec55ee356
u1m0 <caio.quinilato@gmail.com>
2017-02-16 11:24:48 -0200

    Added kibana recipe to install, configure ean restart

--- elasticsearch/elasticsearch-cluster-5.2/cookbooks/kibana/recipes/configure.rb
+++ elasticsearch/elasticsearch-cluster-5.2/cookbooks/kibana/recipes/configure.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+template 'kibana-config' do
+  group                      'kibana'
+  mode                       '0660'
+  owner                      'root'
+  path                       '/etc/kibana/kibana.yml'
+  source                     'kibana.yml.erb'
+  action                     :create
+end
